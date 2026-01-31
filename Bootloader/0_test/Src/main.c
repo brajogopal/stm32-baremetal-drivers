a00edb08b8d15cae93ad3a293e85f50d27c3ba55
brajogopal <brajog1@gmail.com>
2026-01-31 11:17:46 +0530

    Bootloader added

---
+++ Bootloader/0_test/Src/main.c
@@ -0,0 +1,12 @@
+#include "stm32f030x8.h"
+
+
+#define GPIOAEN       (1U<<17)
+unsigned char __attribute__((section(".custom_ram_block"))) custom_ram_buff[10];	//10 bytes
+
+int main(){
+
+	while(1){
+
+	}
+}
