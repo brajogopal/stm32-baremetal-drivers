a7ba313c676c8d4861c8decc2e06bf96edf81068
brajogopal <brajog1@gmail.com>
2026-03-01 16:47:19 +0530

    new sytem driver

---
+++ Bootloader/00_system_driver/Inc/bsp.h
@@ -0,0 +1,13 @@
+/*
+ * bsp.h
+ *
+ *  Created on: 14-Feb-2026
+ *      Author: brajo
+ */
+
+#ifndef BSP_H_
+#define BSP_H_
+
+#include <stdint.h>
+
+#endif /* BSP_H_ */
---
+++ Bootloader/00_system_driver/Src/bsp.c
@@ -0,0 +1,43 @@
+/*
+ * bsp.c
+ *
+ *  Created on: 14-Feb-2026
+ *      Author: brajo
+ */
+#include "bsp.h"
+#include "stm32f030x8.h"
+
+
+typedef enum {
+    INPUT  = 0x0,
+    OUTPUT = 0x1,
+    AF     = 0x2,
+    PWM	   = 0x3
+} mode_t;
+
+typedef enum {
+    GPIOA  = 0x0,
+    GPIOB = 0x1,
+    GPIOC  = 0x2,
+    GPIOD = 0x3
+} port_t;
+
+void pinMode(GPIO_TypeDef *port,uint16_t pin, mode_t mode){
+
+	RCC->AHBENR |=
+
+	port->MODER &= ~(3U << (pin * 2));   // clear bits
+    if (mode == INPUT) {
+        // configure as input
+    	 port->MODER |= (0U << (pin * 2));
+    } else if (mode == OUTPUT) {
+        // configure as output
+    	 port->MODER |= (1U << (pin * 2));
+    } else if (mode == AF) {
+        // configure alternate function
+    	 port->MODER |= (2U << (pin * 2));
+    } else if (mode == PWM) {
+        // configure alternate function
+    	 port->MODER |= (3U << (pin * 2));
+    }
+}
