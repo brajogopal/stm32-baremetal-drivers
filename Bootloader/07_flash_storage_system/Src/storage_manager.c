c60d6fbce17be129f3ce48e4251aff109c03ccd8
brajogopal <brajog1@gmail.com>
2026-05-20 09:11:57 +0530

    updated storage_save in  project2

--- Bootloader/07_flash_storage_system/Src/storage_manager.c
+++ Bootloader/07_flash_storage_system/Src/storage_manager.c
@@ -8,3 +8,36 @@
 #include "storage_manager.h"
 #include "flash_driver.h"
 
+
+
+flash_status_t storage_save(storage_data_t *data){
+
+	flash_status_t status;
+
+
+	status = flash_unlock();
+	if(status != FLASH_OK){
+		flash_lock();
+		return status;
+	}
+
+
+	status = flash_erase_page(STORAGE_PAGE_ADDR);
+	if(status != FLASH_OK){
+		flash_lock();
+		return status;
+	}
+
+
+	status = flash_program_buffer(STORAGE_PAGE_ADDR, (uint16_t*)data ,sizeof(storage_data_t) / 2);
+	if(status != FLASH_OK){
+		flash_lock();
+		return status;
+		}
+
+
+	flash_lock();
+
+	return status;
+
+}
